12a06f174fadaf1f0d3ebf8f9876f63b6231a686
JeongHoHeo <0984aaa@gmail.com>
2023-10-23 17:37:46 +0900

    23 upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azureWebAppTest01.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
